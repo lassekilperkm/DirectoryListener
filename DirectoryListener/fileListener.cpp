de24773295b9d9b5fd211f18daa1c0d50607b09b
Lasse Kilper <lasse.kilper@konicaminolta.de>
2025-03-27 19:57:38 +0100

    removed unused cases

--- DirectoryListener/fileListener.cpp
+++ DirectoryListener/fileListener.cpp
@@ -71,13 +71,9 @@ static BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID l
 			break;
 		case DLL_PROCESS_DETACH:
 			if (listenerThread.joinable())
-			{
 				listenerThread.join();
-			}
 			fclose(listenerLog);
 			break;
-		case DLL_THREAD_ATTACH:
-		case DLL_THREAD_DETACH:
 		default:
 			break;
 	}
